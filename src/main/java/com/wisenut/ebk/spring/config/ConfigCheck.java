778e00cd08855e3a77f6ccccdd49f93073b8cb9c
jenny0122 <asj4459@naver.com>
2024-04-09 11:37:11 +0900

    로그builder 정보 추가

--- src/main/java/com/wisenut/ebk/spring/config/ConfigCheck.java
+++ src/main/java/com/wisenut/ebk/spring/config/ConfigCheck.java
@@ -23,38 +23,51 @@ public class ConfigCheck {
 
     private final ApplicationContext context;
 
-    @Value( "${engine.server.ip}" )
+    @Value("${engine.server.ip}")
     String server_ip;
 
-    @PostConstruct
-    public void checkConnectionEngine( ) {
+    final int SERVER_PORT = 7000;
 
-        QueryAPI530.Search search = new QueryAPI530.Search( );
+    final int TIMEOUT = 10 * 1000;
+
+    @PostConstruct
+    public void checkConnectionEngine() {
+
+        QueryAPI530.Search search = new QueryAPI530.Search();
         int ret = 0;
 
-        ret = search.w3ConnectServer( server_ip , 7000 , 10 * 1000 );
+        StringBuilder logBuilder = new StringBuilder();
+        log.info("**********************************");
+        logBuilder.append("  SearchEngine Connection Test..." + "\n");
+        logBuilder.append("\tip: " + server_ip + "\n");
+        logBuilder.append("\tport: " + SERVER_PORT + "\n");
+        logBuilder.append("\ttimeout: " + TIMEOUT + "\n");
+        log.info(logBuilder.toString());
+        log.info("**********************************");
 
-        if ( ret != 0 ) {
-            log.error( "연결 실패" );
+        ret = search.w3ConnectServer(server_ip, 7000, 10 * 1000);
+
+        if (ret != 0) {
+            log.error("연결 실패");
 //            System.exit( SpringApplication.exit( context , ( ) -> 0 ) );
         }
     }
 
     @Bean
-    public ConfigurableServletWebServerFactory configurableServletWebServerFactory( ) {
-        return new TomcatServletWebServerFactory( ) {
+    public ConfigurableServletWebServerFactory configurableServletWebServerFactory() {
+        return new TomcatServletWebServerFactory() {
             @Override
-            protected void postProcessContext( Context context ) {
-                super.postProcessContext( context );
-                JspPropertyGroup jspPropertyGroup = new JspPropertyGroup( );
-                jspPropertyGroup.addUrlPattern( "*.jsp" );
-                jspPropertyGroup.setPageEncoding( "UTF-8" );
-                jspPropertyGroup.setScriptingInvalid( "false" );
-                jspPropertyGroup.addIncludePrelude( "/WEB-INF/jsp/common/common.jsp" );
-                jspPropertyGroup.setTrimWhitespace( "true" );
-                jspPropertyGroup.setDefaultContentType( "text/html" );
-                JspPropertyGroupDescriptorImpl jspPropertyGroupDescriptor = new JspPropertyGroupDescriptorImpl( jspPropertyGroup );
-                context.setJspConfigDescriptor( new JspConfigDescriptorImpl( Collections.singletonList( jspPropertyGroupDescriptor ) , Collections.emptyList( ) ) );
+            protected void postProcessContext(Context context) {
+                super.postProcessContext(context);
+                JspPropertyGroup jspPropertyGroup = new JspPropertyGroup();
+                jspPropertyGroup.addUrlPattern("*.jsp");
+                jspPropertyGroup.setPageEncoding("UTF-8");
+                jspPropertyGroup.setScriptingInvalid("false");
+                jspPropertyGroup.addIncludePrelude("/WEB-INF/jsp/common/common.jsp");
+                jspPropertyGroup.setTrimWhitespace("true");
+                jspPropertyGroup.setDefaultContentType("text/html");
+                JspPropertyGroupDescriptorImpl jspPropertyGroupDescriptor = new JspPropertyGroupDescriptorImpl(jspPropertyGroup);
+                context.setJspConfigDescriptor(new JspConfigDescriptorImpl(Collections.singletonList(jspPropertyGroupDescriptor), Collections.emptyList()));
             }
         };
     }
